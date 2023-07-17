c47dcbfd8f9beca4f937ca285c5beec360d50d85
Hunesu Kim <kewcxea@hanmail.net>
2023-07-17 23:56:14 +0900

    0717 Spring AOP를 이용한 로그 추적기 구현

--- src/main/java/BoardAdv/AnonymLog/aop/aspect/LogAspect.java
+++ src/main/java/BoardAdv/AnonymLog/aop/aspect/LogAspect.java
@@ -1,18 +1,34 @@
 package BoardAdv.AnonymLog.aop.aspect;
 
+import BoardAdv.AnonymLog.logtracer.TraceStatus;
+import BoardAdv.AnonymLog.logtracer.logtrace.LogTrace;
+import BoardAdv.AnonymLog.logtracer.logtrace.LogTraceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
 @Slf4j
+@Component
 public class LogAspect {
+
+    private final LogTrace logTrace=new LogTraceImpl();
 
     @Around("BoardAdv.AnonymLog.aop.pointcuts.LogPointcut.unionPointcut()")
     public Object writeLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("==========START : {}", joinPoint.getSignature());
-        return joinPoint.proceed();
+        TraceStatus status = logTrace.begin(joinPoint.getSignature().getName());
+        try{
+            joinPoint.proceed();
+            logTrace.end(status);
+            return joinPoint.proceed();
+        } catch (Exception e){
+            logTrace.exception(status,e);
+            throw e;
+        }
+
     }
 
 }
